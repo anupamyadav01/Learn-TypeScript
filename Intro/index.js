f03897a8f9fa088d4ebc0c3d49aa3f8aa09c9598
Anupam Yadav <anupamy571@gmail.com>
2024-08-11 11:51:20 +0530

    Introduction of typescript

---
+++ Intro/index.js
@@ -0,0 +1,19 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greetings = "Hello..";
+var num = 12;
+console.log(greetings);
+// function in typescript
+var add = function (firstNumber) {
+    console.log(firstNumber);
+};
+// add(23)
+var getUpperCase = function (val) {
+    return val.toUpperCase();
+};
+var login = function (userName, email, password) {
+    return "2342";
+};
+login("anupam", "anu.co@dev", 9230923490);
+var returnNothing = function (name) { };
+getUpperCase("anupamyadav");
---
+++ Intro/index.ts
@@ -0,0 +1,25 @@
+const greetings: string = "Hello..";
+const num : number = 12;
+
+console.log(greetings);
+
+// function in typescript
+const add = (firstNumber : number) => {
+    console.log(firstNumber);
+    
+}
+// add(23)
+const getUpperCase = (val : string) => {
+    return val.toUpperCase();
+}
+const login = (userName: string, email: string, password: number) : string => {
+    return "2342"
+}
+login("anupam", "anu.co@dev", 9230923490)
+
+const returnNothing = (name : string) : void=> {}
+getUpperCase("anupamyadav")
+
+// next video timing 01.15.00
+
+export {}
