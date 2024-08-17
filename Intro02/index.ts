8d556fc9c2838e64189bd0f289246fdb4ce6edbb
Anupam Yadav <anupamy571@gmail.com>
2024-08-17 23:27:35 +0530

    More about  functions

---
+++ Intro02/index.ts
@@ -0,0 +1,12 @@
+// Type aliases 
+type User = {
+    name : string;
+    email : string;
+    age : number;
+    isActive : boolean
+}
+const createUser = (user : User) => {
+
+}
+const newUser = 
+createUser()
