e7b7216731a2fcfa3bab1d4d2028301b671aba2e
Anupam Yadav <anupamy571@gmail.com>
2024-08-20 11:23:14 +0530

    unions

--- Intro02/unions.ts
+++ Intro02/unions.ts
@@ -23,3 +23,28 @@ const anupam: User | Admin = {
 // }
 
 // getId(1231);
+
+// unions with array 
+const data: number[] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+const data2: string[] = ["anupam", "dev"]
+
+// either number or string
+
+const data3: (string | number)[] = ["anupam", 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+
+// for stricty typing
+
+let pi: 3.14 = 3.14; // we cannot change it further in future
+
+// example 
+
+let seatAllotment: "aisle" | "middle" | "window"
+
+seatAllotment = "aisle" // we can change it
+seatAllotment = "window"
+
+// but
+
+// seatAllotment = "center" // we cannot change it
